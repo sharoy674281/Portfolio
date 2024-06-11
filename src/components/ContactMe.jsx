2f93346cf5f998ef5323c337995c50dfa53dc297
rakhman <Rakhmanirs@yahoo.no>
2024-06-11 16:41:51 +0200

    contact form fixed

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 const ContactMe = () => {
+	const form = useRef();
+
+	const sendEmail = (e) => {
+		e.preventDefault();
+		emailjs
+			.sendForm("service_iud0x8o", "template_cw39sah", form.current, {
+				publicKey: "CECFgAHaLJcOqq3jj",
+			})
+			.then(
+				() => {
+					console.log("SUCCESS!");
+				},
+				(error) => {
+					console.log("FAILED...", error.text);
+				}
+			);
+	};
 	return (
 		<div
 			id="contact-me"
@@ -9,21 +27,35 @@ const ContactMe = () => {
 				Contact <span className="text-lightBlue">Me</span>
 			</h1>
 			<form
+				ref={form}
+				onSubmit={sendEmail}
 				action=""
-				method="get"
 				className="mt-8 flex flex-col items-center text-white">
 				<div className="grid lg:grid-cols-2 gap-2 contact-inputs">
-					<input required type="text" placeholder="Full Name" />
-					<input required type="email" placeholder="Email Address" />
+					<input
+						required
+						type="text"
+						placeholder="Full Name"
+						name="from_name"
+					/>
+					<input
+						required
+						type="email"
+						placeholder="Email Address"
+						name="user_email"
+					/>
 					<input required type="number" placeholder="Mobile Number" />
 					<input required type="text" placeholder="Email Subject" />
 				</div>
 				<textarea
 					required
-					name=""
+					name="message"
 					id=""
 					className="resize-none w-full rounded-md border-[2px] border-lightBlue mt-2 h-64 pl-2 pt-2 mb-2 bg-shade"></textarea>
-				<button type="submit" className="button1 max-lg:mt-3 max-lg:h-10">
+				<button
+					type="submit"
+					className="button1 max-lg:mt-3 max-lg:h-10 "
+					value="Send">
 					Submit
 				</button>
 			</form>
